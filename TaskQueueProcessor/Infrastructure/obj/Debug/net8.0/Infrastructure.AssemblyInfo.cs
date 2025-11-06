ccbd8ad8e643adc985f15be292321aeb86b07918
Aamir Hameed <aamir.hameed7@gmail.com>
2025-11-06 14:12:29 +0500

    init update

--- TaskQueueProcessor/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ TaskQueueProcessor/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9440e505893950027e6d014a3d71211b429d6333")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
